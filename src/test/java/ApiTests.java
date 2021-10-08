b019cfa79184584b99a2fc10eec773bf0d589855
Mhabibullin <habib@e1.ru>
2021-10-08 11:42:21 +0500

    listOfUsersTest added

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,12 +1,22 @@
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTests {
 
     @Test
-    void singleUserTest() {
+    void listOfUsersTest(){
+        when().
+                get("https://reqres.in/api/unknown").
+                then().
+                body("data.findAll { it.year < 2002 }.name", hasItems("fuchsia rose", "cerulean"));
 
+    }
+
+  /*  void singleUserTest() {
         given()
                 .get("https://reqres.in/api/users/2")
                 .then()
@@ -15,5 +25,5 @@ public class ApiTests {
 
 
     }
-
+*/
 }
