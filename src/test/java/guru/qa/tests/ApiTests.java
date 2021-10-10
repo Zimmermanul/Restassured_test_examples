1514a1441ac09a5a1a1b472f88579d296c971f8d
Mhabibullin <habib@e1.ru>
2021-10-10 18:47:24 +0500

    CreateUserTest added

--- src/test/java/guru/qa/tests/ApiTests.java
+++ src/test/java/guru/qa/tests/ApiTests.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import guru.qa.models.User;
 import guru.qa.models.UserData;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ import java.util.Optional;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 
@@ -66,11 +67,26 @@ public class ApiTests {
                 .statusCode(200)
                 .body("data.findAll { it.year < 2002 }.name", hasItems("fuchsia rose", "cerulean"));
     }
+
     @Test
     void singleUserTest() {
         get("/users/2")
                 .then()
                 .statusCode(200)
                 .body("data.first_name", is("Janet"));
+    }
+
+    @Test
+    void createUserTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(this.userData.toString())
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("job", equalTo(job))
+                .body("id", notNullValue());
     }
 }
